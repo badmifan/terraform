87b346c2f1837e528cc6dce7f52f1cbe9cfd8959
mif <bad.mifan@gmail.com>
2020-01-12 22:23:39 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,46 @@
+// Configure the Google Cloud provider
+provider "google" {
+  credentials = "${file("test.json")}"
+  project     = "able-yew-254816"
+  region      = "us-west1"
+}
+
+resource "google_compute_instance" "default" {
+  name = "elk"
+  //machine_type = "f1-micro"
+  machine_type = "n1-standard-1"
+  zone         = "us-west1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+    }
+  }
+
+  // Make sure flask is installed on all new instances for later steps
+  //metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
+  metadata_startup_script = "${file("startup.sh")}"
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
+
+  //provisioner "file" {
+  //  source      = "script.sh"
+  //  destination = "/tmp/script.sh"
+  //}
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "elk"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5601", "5044"]
+  }
+}
