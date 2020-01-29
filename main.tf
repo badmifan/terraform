17238652c1a7261c52e3d55271fa9ae94ce8aff3
mif <bad.mifan@gmail.com>
2020-01-30 00:34:51 +0200

    add new terraform configuration

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ provider "google" {
 }
 
 resource "google_compute_instance" "default" {
-  name = "elk"
-  //machine_type = "f1-micro"
-  machine_type = "n1-standard-1"
-  zone         = "us-west1-a"
+  name         = "cm"
+  machine_type = "f1-micro"
+  //machine_type = "n1-standard-1"
+  zone = "us-west1-a"
 
   boot_disk {
     initialize_params {
@@ -20,7 +20,7 @@ resource "google_compute_instance" "default" {
   // Make sure flask is installed on all new instances for later steps
   //metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
   metadata = {
-    ssh-keys = "mikhaild:${file("~/.ssh/id_rsa.pub")}"
+    ssh-keys = "mif:${file("~/.ssh/id_rsa.pub")}"
   }
 
   metadata_startup_script = "${file("startup.sh")}"
@@ -33,26 +33,93 @@ resource "google_compute_instance" "default" {
     }
   }
 
+  # provisioner "local-exec" {
+  #   command = "sed -i.bak 's/master_ip/${network_interface.0.network_ip}/g' config.ini my.cnf"
+  # }
+
   provisioner "file" {
-    source      = "demo.conf"
-    destination = "/tmp/demo.conf"
+    source      = "files"
+    destination = "/tmp/files"
 
     connection {
       host        = self.network_interface.0.access_config.0.nat_ip
       type        = "ssh"
-      user        = "mikhaild"
+      user        = "mif"
       private_key = "${file("~/.ssh/id_rsa")}"
       agent       = false
     }
   }
-}
 
-resource "google_compute_firewall" "default" {
-  name    = "elk"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["5601", "5044"]
+  provisioner "remote-exec" {
+    connection {
+      host        = self.network_interface.0.access_config.0.nat_ip
+      type        = "ssh"
+      user        = "mif"
+      private_key = "${file("~/.ssh/id_rsa")}"
+      agent       = false
+    }
+    inline = [
+      "cd /tmp/files",
+      "sudo apt-get update",
+      "sudo apt install libaio1 libmecab2",
+      "sed -i.bak 's/master_ip/${self.network_interface.0.network_ip}/g' config.ini my.cnf",
+      "cd ..",
+      "wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-8.0/mysql-cluster_8.0.19-1ubuntu18.04_amd64.deb-bundle.tar",
+      "mkdir install",
+      "tar -xvf mysql-cluster_8.0.19-1ubuntu18.04_amd64.deb-bundle.tar -C install/",
+      "cd install",
+      "sudo dpkg -i mysql-cluster-community-management-server_8.0.19-1ubuntu18.04_amd64.deb",
+      "sudo mkdir /var/lib/mysql-cluster",
+      "sudo cp /tmp/files/config.ini /var/lib/mysql-cluster/config.ini",
+      "sudo cp /tmp/files/ndb_mgmd.service /etc/systemd/system/ndb_mgmd.service",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl enable ndb_mgmd",
+      "sudo systemctl start ndb_mgmd",
+      //      "consul join ${aws_instance.web.private_ip}",
+    ]
   }
+
+
 }
+
+
+# sudo apt-get update &&
+# #wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-8.0/mysql-cluster-community-management-server_8.0.19-1ubuntu18.04_amd64.deb
+# #sudo dpkg -i mysql-cluster-community-management-server_8.0.19-1ubuntu18.04_amd64.deb
+# sudo mkdir /var/lib/mysql-cluster
+# wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-8.0/mysql-cluster_8.0.19-1ubuntu18.04_amd64.deb-bundle.tar
+# mkdir install
+# tar -xvf mysql-cluster_8.0.19-1ubuntu18.04_amd64.deb-bundle.tar -C install/
+# cd install
+# #sudo apt update
+# sudo apt install libaio1 libmecab2
+#sudo dpkg -i mysql-common_8.0.19-1ubuntu18.04_amd64.deb
+#sudo dpkg -i mysql-cluster-community-client-core_8.0.19-1ubuntu18.04_amd64.deb
+#sudo dpkg -i mysql-cluster-community-client_8.0.19-1ubuntu18.04_amd64.deb 
+#sudo dpkg -i mysql-client_8.0.19-1ubuntu18.04_amd64.deb 
+#sudo dpkg -i mysql-cluster-community-server-core_8.0.19-1ubuntu18.04_amd64.deb
+
+#sudo debconf-set-selections <<< "mysql-cluster-community-server mysql-cluster-community-server/root-pass password mypassword"
+#sudo debconf-set-selections <<< "mysql-cluster-community-server mysql-cluster-community-server/re-root-pass password mypassword"
+#sudo debconf-set-selections <<< "mysql-cluster-community-server mysql-server/default-auth-override      select  Use Legacy Authentication Method (Retain MySQL 5.x Compatibility)"
+
+#sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-cluster-community-server_8.0.19-1ubuntu18.04_amd64.deb
+#sudo dpkg -i mysql-server_8.0.19-1ubuntu18.04_amd64.deb 
+# sudo cp /tmp/files/config.ini /var/lib/mysql-cluster/config.ini
+# sudo cp /tmp/files/ndb_mgmd.service /etc/systemd/system/ndb_mgmd.service
+# sudo systemctl daemon-reload
+# sudo systemctl enable ndb_mgmd
+# sudo systemctl start ndb_mgmd
+# sudo systemctl status ndb_mgmd
+
+
+
+# resource "google_compute_firewall" "default" {
+#   name    = "elk"
+#   network = "default"
+
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["5601", "5044"]
+#   }
+# }
--- startup.sh
+++ startup.sh
@@ -1,18 +1,30 @@
 #!/bin/bash -x
-sudo apt-get update &&
-sudo apt-get install -y default-jre &&
-wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - &&
-sudo apt-get install apt-transport-https &&
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list &&
-sudo apt-get update && sudo apt-get install -y elasticsearch logstash kibana &&
-sudo systemctl daemon-reload 
-sudo systemctl enable elasticsearch.service
-sudo bash -c 'echo network.host: 127.0.0.1 >> /etc/elasticsearch/elasticsearch.yml' 
-sudo systemctl restart elasticsearch.service
-sudo bash -c 'echo server.host: 0.0.0.0 >> /etc/kibana/kibana.yml' 
-sudo systemctl enable kibana.service
-sudo systemctl restart kibana.service
-sudo systemctl enable logstash.service
-sudo cp /tmp/demo.conf /etc/logstash/conf.d/demo.conf
-sudo systemctl restart logstash.service
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
 
+#cd /tmp
+#sudo apt-get update
+#sudo apt install libaio1 libmecab2
+#wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-8.0/mysql-cluster-community-management-server_8.0.19-1ubuntu18.04_amd64.deb
+#sudo dpkg -i mysql-cluster-community-management-server_8.0.19-1ubuntu18.04_amd64.deb
+#
+#sudo dpkg -i mysql-common_8.0.19-1ubuntu18.04_amd64.deb
+#sudo dpkg -i mysql-cluster-community-client-core_8.0.19-1ubuntu18.04_amd64.deb
+#sudo dpkg -i mysql-cluster-community-client_8.0.19-1ubuntu18.04_amd64.deb 
+#sudo dpkg -i mysql-client_8.0.19-1ubuntu18.04_amd64.deb 
+#sudo dpkg -i mysql-cluster-community-server-core_8.0.19-1ubuntu18.04_amd64.deb
+
+#sudo debconf-set-selections <<< "mysql-cluster-community-server mysql-cluster-community-server/root-pass password mypassword"
+#sudo debconf-set-selections <<< "mysql-cluster-community-server mysql-cluster-community-server/re-root-pass password mypassword"
+#sudo debconf-set-selections <<< "mysql-cluster-community-server mysql-server/default-auth-override      select  Use Legacy Authentication Method (Retain MySQL 5.x Compatibility)"
+
+#sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-cluster-community-server_8.0.19-1ubuntu18.04_amd64.deb
+#sudo dpkg -i mysql-server_8.0.19-1ubuntu18.04_amd64.deb 
+# sudo cp /tmp/files/config.ini /var/lib/mysql-cluster/config.ini
+# sudo cp /tmp/files/ndb_mgmd.service /etc/systemd/system/ndb_mgmd.service
+# sudo systemctl daemon-reload
+# sudo systemctl enable ndb_mgmd
+# sudo systemctl start ndb_mgmd
+# sudo systemctl status ndb_mgmd
